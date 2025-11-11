9e5ccd93ba50f7097e5437d6a11753e80e856bba
laynaria <pro.laynaria@outlook.fr>
2025-11-11 22:09:28 +0100

    made a get route

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,24 @@ app.use(express.json());
 
 const port: number = 3000;
 
+export const dataArray = [
+  { id: 1, name: "azerty" },
+  { id: 2, name: "object" },
+  { id: 3, name: "waffle" },
+];
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.get("/data", (req: Request, res: Response) => {
+  res.send(dataArray);
+});
+
+app.get("/data/:id", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+
+  res.send(dataArray[dataArray.findIndex((data) => data.id === id)]);
 });
 
 app.listen(port, () => {
